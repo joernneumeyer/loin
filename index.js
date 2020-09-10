bb2c5f6ed676f765c5c7173ff5f4703f56abed2b
Jörn Neumeyer <contact@joern-neumeyer.de>
2020-09-10 15:28:01 +0200

    local package installation works now

--- index.js
+++ index.js
@@ -13,23 +13,23 @@
 // You should have received a copy of the GNU General Public License
 // along with loin.  If not, see <https://www.gnu.org/licenses/>.
 
-const fs   = require('fs');
+const fs = require('fs');
 const path = require('path');
 const {
-        removePackage,
-        installPackage,
-      }    = require('./lib');
+  removePackage,
+  installPackage,
+} = require('./lib');
 
 if (process.argv.length < 3) {
   console.error('Please provide at least one argument!');
   return 1;
 }
 
-const projectDirectory  = '/home/anon/Code/github-repos/loin';// process.argv[1];
+const projectDirectory = process.argv[1];
 const projectConfigPath = path.join(projectDirectory, '.loin');
 
 const CMD_INSTALL = 0x00000001;
-const CMD_REMOVE  = 0x00000010;
+const CMD_REMOVE = 0x00000010;
 
 let cmd = 0;
 switch (process.argv[2]) {
@@ -54,19 +54,20 @@ if (!fs.existsSync(projectConfigPath)) {
 const config   = fs.readFileSync(projectConfigPath).toString('utf8');
 const packages = config.packages || {};
 
-(async() => {
+(async () => {
   if (cmd & CMD_REMOVE) {
 
   }
   if (cmd & CMD_INSTALL) {
     if (process.argv[3]) {
-      const packagePath =
-              process.argv[3].startsWith('/')
-              ? process.argv[3]
-              : path.join(projectConfigPath, process.argv[3]);
+      if (process.argv[3].startsWith('/')) {
+        console.error('Please provide a relative path!');
+        process.exit(1);
+      }
+      const packagePath = process.argv[3];
       console.log('Starting installation...');
-      const pack = await installPackage(projectConfigPath, packagePath);
-      console.log(`Successfully installed package '${pack.name}'.`);
+      const packageName = await installPackage(projectDirectory, packagePath);
+      console.log(`Successfully installed package '${packageName}'.`);
     }
   }
 })();
--- lib.js
+++ lib.js
@@ -13,24 +13,41 @@
 // You should have received a copy of the GNU General Public License
 // along with loin.  If not, see <https://www.gnu.org/licenses/>.
 
-const fs   = require('fs');
+const fs = require('fs');
 const path = require('path');
 const util = require('util');
 const { copy } = require('fs-extra');
 
 const fileExists = util.promisify(fs.exists);
 
-async function installPackage(projectPath, packagePath) {
+function copyFilter(src, dest) {
+  return !src.includes('node_modules');
+}
+
+async function installPackage(projectPath, relativePackagePath) {
+
+  const packagePath = path.join(projectPath, relativePackagePath);
   const packPath = path.join(packagePath, 'package.json');
+  const packageName =
+    await fs.promises.readFile(packPath)
+      .then(x => x.toString('utf8'))
+      .then(x => JSON.parse(x).name);
+  const loinFolder = path.join(projectPath, 'loin_modules', packageName);
+  await copy(packagePath, loinFolder, { overwrite: true, filter: copyFilter });
+  const projectPackPath = path.join(projectPath, 'package.json');
   const pack =
-          await fs.promises.readFile(packPath)
-                  .then(x => x.toString('utf8'))
-                  .then(x => JSON.parse(x));
-  const loinFolder = path.join(projectPath, 'loin_modules', pack.name);
-  await copy(pack, loinFolder);
-  pack.dependencies[pack.name] = 'file:loin_modules/' + pack.name;
-  await fs.promises.writeFile(packPath, JSON.stringify(pack, undefined, 2));
-  return pack;
+    await fs.promises.readFile(projectPackPath)
+      .then(x => x.toString('utf8'))
+      .then(x => JSON.parse(x));
+  pack.dependencies[packageName] = 'file:loin_modules/' + packageName;
+  await fs.promises.writeFile(projectPackPath, JSON.stringify(pack, undefined, 2));
+  const loinConfig = await fs.promises.readFile(path.join(projectPath, '.loin')).then(x => JSON.parse(x.toString('utf-8')));
+  if (!loinConfig.packages) {
+    loinConfig.packages = {};
+  }
+  loinConfig.packages[packageName] = relativePackagePath;
+  await fs.promises.writeFile(path.join(projectPath, '.loin'), JSON.stringify(loinConfig, undefined, 2));
+  return packageName;
 }
 
 async function removePackage(a) {
